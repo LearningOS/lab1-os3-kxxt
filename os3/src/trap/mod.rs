b0504d18c2f53cbc82bcb94882a131082eae2b5b
kxxt <rsworktech@outlook.com>
2022-11-08 22:52:25 +0800

    impl trap asm

--- os3/src/trap/mod.rs
+++ os3/src/trap/mod.rs
@@ -1,3 +1,17 @@
 mod context;
 
+use core::arch::global_asm;
+
 pub use context::TrapContext;
+use riscv::register::{stvec, utvec::TrapMode};
+
+global_asm!(include_str!("trap.S"));
+
+pub fn init() {
+    extern "C" {
+        fn __alltraps();
+    }
+    unsafe {
+        stvec::write(__alltraps as usize, TrapMode::Direct);
+    }
+}
