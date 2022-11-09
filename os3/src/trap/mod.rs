96db4438e2d9d0c661bbe50926239962aa889f93
kxxt <rsworktech@outlook.com>
2022-11-09 08:37:47 +0800

    impl trap_handler

--- os3/src/trap/mod.rs
+++ os3/src/trap/mod.rs
@@ -3,7 +3,7 @@ mod context;
 use core::arch::global_asm;
 
 pub use context::TrapContext;
-use riscv::register::{stvec, utvec::TrapMode};
+use riscv::register::{scause, stval, stvec, utvec::TrapMode};
 
 global_asm!(include_str!("trap.S"));
 
@@ -14,4 +14,31 @@ pub fn init() {
     unsafe {
         stvec::write(__alltraps as usize, TrapMode::Direct);
     }
+}
+
+pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
+    let scause = scause::read();
+    let stval = stval::read();
+    use scause::Exception::*;
+    use scause::Trap::Exception;
+    match scause.cause() {
+        Exception(UserEnvCall) => {
+            cx.sepc += 4;
+            cx.x[10] = syscall(cx.x[17], [cx.x[10], cx.x[11], cx.x[12]]) as usize;
+        }
+        Exception(StoreFault) | Exception(StorePageFault) => {
+            error!("[kernel] Store(Page)Fault in application, core dumped.");
+            run_next_app();
+        }
+        Exception(IllegalInstruction) => {
+            error!("[kernel] IllegalInstruction in application, core dumped.");
+            run_next_app();
+        }
+        _ => panic!(
+            "Unsupported trap {:?}, stval = {:#x}!",
+            scause.cause(),
+            stval
+        ),
+    }
+    cx
 }
